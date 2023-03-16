b57a6cb10567c9bd34df36571a781f7a0d9b0bc3
cortedteate <carlos.cortesosorio@teatedigital.com>
2023-03-16 14:02:13 -0500

    Fix Devise views for login and created modules for categories, suppliers and clients

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,61 @@
+class ClientsController < ApplicationController
+    before_action :set_client, only: [:edit, :update, :destroy]
+
+    def index
+        @clients = Client.all.order(:id)
+      end
+    
+      def new
+        @client = Client.new 
+      end
+    
+      def edit
+      end
+    
+      def create
+        @client = Client.new(client_params)
+    
+        respond_to do |format|
+          if @client.save 
+            format.json { head :no_content }
+            format.js
+          else 
+            format.json { render json: @client.errors.full_messages, status: :unprocessable_entity }
+            format.js { render :new }
+          end
+        end
+      end
+    
+      def update
+        respond_to do |format|
+          if @client.update(client_params) 
+            format.json { head :no_content }
+            format.js
+          else 
+            format.json { render json: @client.errors.full_messages, status: :unprocessable_entity }
+            format.js { render :edit }
+          end
+        end
+      end
+    
+      def destroy
+        @client.destroy
+        respond_to do |format|
+          format.json { head :no_content }
+          format.js
+        end
+      end
+    
+    
+      private
+    
+      def set_client
+        @client = Client.find(params[:id])
+      end
+    
+      def client_params
+        params.require(:client).permit(:name, :address, :phone)
+      end
+    
+
+end
